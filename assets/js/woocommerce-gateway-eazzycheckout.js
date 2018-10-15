d3e85583fc64d58059a1dec71a78ce3389827ab8
Kiberu Sharif <kiberusharif@gmail.com>
2018-10-15 14:48:04 +0300

    ready for staging

--- assets/js/woocommerce-gateway-eazzycheckout.js
+++ assets/js/woocommerce-gateway-eazzycheckout.js
@@ -15,22 +15,21 @@
 			var expiry = "2030-02-17T19:00:00";
 
 			var gateway_form = $('#eazzycheckout-payment-form');
-			gateway_form.append('<input type="text" id="token" name="token" value="' + token + '">');
-			gateway_form.append('<input type="text" id="amount" name="amount" value="' + amount + '">');
-			gateway_form.append('<input type="text" id="orderReference" name="orderReference" value="' + orderRef + '">');
-			gateway_form.append('<input type="text" id="merchantCode" name="merchantCode" value="' + merchantCode + '">');
-			gateway_form.append('<input type="text" id="outletCode" name="outletCode" value="' + outletCode + '">');
-			gateway_form.append('<input type="text" id="currency" name="currency" value="' + currency + '">');
-			gateway_form.append('<input type="text" id="popupLogo" name="popupLogo" value="' + popupLogo + '">');
-			gateway_form.append('<input type="text" id="custName" name="custName" value="' + custName + '">');
-			gateway_form.append('<input type="text" id="ez1_callbackurl" name="ez1_callbackurl" value="' + ez1_callbackurl + '">');
-			gateway_form.append('<input type="text" id="expiry" name="expiry" value="' + expiry + '">');
-			gateway_form.append('<input type="text" id="website" name="website" value="' + website + '">');
-			gateway_form.append('<input type="text" id="description" name="description" value="' + description + '">');
-			gateway_form.append('<input type="text" id="merchant" name="merchant" value="Kasha">');
-			gateway_form.append('<input type="submit" value="submit">');
+			gateway_form.append('<input type="hidden" id="token" name="token" value="' + token + '">');
+			gateway_form.append('<input type="hidden" id="amount" name="amount" value="' + amount + '">');
+			gateway_form.append('<input type="hidden" id="orderReference" name="orderReference" value="' + orderRef + '">');
+			gateway_form.append('<input type="hidden" id="merchantCode" name="merchantCode" value="' + merchantCode + '">');
+			gateway_form.append('<input type="hidden" id="outletCode" name="outletCode" value="' + outletCode + '">');
+			gateway_form.append('<input type="hidden" id="currency" name="currency" value="' + currency + '">');
+			gateway_form.append('<input type="hidden" id="popupLogo" name="popupLogo" value="' + popupLogo + '">');
+			gateway_form.append('<input type="hidden" id="custName" name="custName" value="' + custName + '">');
+			gateway_form.append('<input type="hidden" id="ez1_callbackurl" name="ez1_callbackurl" value="' + ez1_callbackurl + '">');
+			gateway_form.append('<input type="hidden" id="expiry" name="expiry" value="' + expiry + '">');
+			gateway_form.append('<input type="hidden" id="website" name="website" value="' + website + '">');
+			gateway_form.append('<input type="hidden" id="description" name="description" value="' + description + '">');
+			gateway_form.append('<input type="hidden" id="merchant" name="merchant" value="Kasha">');
 
-			// gateway_form.submit();
+			gateway_form.submit();
 
 		}
 	);
